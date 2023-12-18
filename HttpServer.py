960d073fbc42759e4e17d72a7ca578b6264dff0a
LiaoChenyi <101239523+LiaoChenyi@users.noreply.github.com>
2023-12-18 22:55:08 +0800

    implement View and Download part

--- HttpServer.py
+++ HttpServer.py
@@ -18,6 +18,10 @@ import traceback
 import json
 import base64
 
+# other module can be used according to https://github.com/Leosang-lx/SUSTech-CS305-2023Fall
+import re
+
+
 class ClientAccount:
     def __init__(self, username, password):
         self.username = username
@@ -26,10 +30,12 @@ class ClientAccount:
 
     def getBase64(self):
         return base64.b64encode('{}:{}'.format(self.username, self.password).encode('utf-8')).decode('utf-8')
-    
+
+
 clients = [ClientAccount('client1', '123'),
            ClientAccount('client2', '123'),
            ClientAccount('client3', '123')]
+
 
 class HttpServer:
     def __init__(self, client_socket, addr):
@@ -47,50 +53,58 @@ class HttpServer:
 
     def handle_request(self):
         # while True:
-            request = self.client_socket.recv(1024).decode('utf-8')
-            print('Request: {}'.format(request))
+        request = self.client_socket.recv(1024).decode('utf-8')
+        print('Request: {}'.format(request))
 
-            # parse the request
-            request_body = request.split('\r\n\r\n', 1)[1]
-            request = request.split('\r\n')
-            request_line = request[0].split(' ')
-            method = request_line[0]
-            path = request_line[1]
-            http_version = request_line[2]
+        # parse the request
+        request_body = request.split('\r\n\r\n', 1)[1]
+        request = request.split('\r\n')
+        request_line = request[0].split(' ')
+        method = request_line[0]
+        path = request_line[1]
+        param = ''
+        if '?' in path:
+            path, param = path.split('?', 1)
+        http_version = request_line[2]
 
-            headers = {}
-            for line in request[1:-2]:
-                key, value = line.split(': ')
-                headers[key] = value
+        headers = {}
+        for line in request[1:-2]:
+            key, value = line.split(': ')
+            headers[key] = value
 
-            if 'Authorization' not in headers:
-                header = 'WWW-Authenticate: Basic realm=Basic realm="Authorization Required"\r\n'
-                self.handle_error(401, 'Unauthorized', headers=header)
-                return
-            Authertication = headers['Authorization']
-            base64 = Authertication.split(' ')[-1]
-            if base64 not in [client.base64 for client in clients]:
-                # self.send_response(401)
-                self.handle_error(401, 'Unauthorized')
-                return
+        if 'Authorization' not in headers:
+            header = 'WWW-Authenticate: Basic realm=Basic realm="Authorization Required"\r\n'
+            self.handle_error(401, 'Unauthorized', headers=header)
+            return
+        Authertication = headers['Authorization']
+        base64 = Authertication.split(' ')[-1]
+        if base64 not in [client.base64 for client in clients]:
+            # self.send_response(401)
+            self.handle_error(401, 'Unauthorized')
+            return
 
-            # handle the request
-            if method == 'GET':
-                self.handle_get(path)
-            elif method == 'POST':
-                self.handle_post(path, request[-1])
-            elif method == 'HEAD':
-                self.handle_head(path)
-            else:
-                self.handle_error(405, 'Method Not Allowed')
-            # if 'Connection' in headers:
-            #     if headers['Connection'] == 'close':
-            #         self.client_socket.close()
-            #         break
+        # handle the request
+        if method == 'GET':
+            self.handle_get(path, param)
+        elif method == 'POST':
+            self.handle_post(path, request[-1])
+        elif method == 'HEAD':
+            self.handle_head(path, param)
+        else:
+            self.handle_error(405, 'Method Not Allowed')
+        # if 'Connection' in headers:
+        #     if headers['Connection'] == 'close':
+        #         self.client_socket.close()
+        #         break
 
-    def handle_get(self, path):
+    def handle_get(self, path, param):
         print(path)
-        origin_path = ''
+        # if it is url format
+        http_url_pattern = re.compile(r'^(/[^/]*)*(\?.*)?$')
+        if not bool(re.match(http_url_pattern, path)):
+            self.handle_error(400, 'Bad Request')
+            return
+
         # modify to actual path
         if path == '/':
             origin_path = 'data'
@@ -99,21 +113,28 @@ class HttpServer:
 
         # if the path is a directory, return the index.html file
         if os.path.isdir(origin_path):
-            response_body = list_directory_html(origin_path, path)
-            response = f'HTTP/1.1 200 OK\r\n\r\n{response_body}'
-            self.client_socket.sendall(response.encode("utf-8"))
+            if param == 'SUSTech-HTTP=0' or param == '':
+                self.handle_dir(origin_path, path, True)
+            elif param == 'SUSTech-HTTP=1':
+                self.handle_dir(origin_path, path, False)
+            else:
+                self.handle_error(400, 'Bad Request')
         # if the path is a file, return the file
         elif os.path.isfile(origin_path):
-            self.handle_file(path)
+            self.handle_file(origin_path)
         else:
             self.handle_error(404, 'Not Found')
 
     def handle_post(self, path, data):
         return
-    
-    def handle_head(self, path):
+
+    def handle_head(self, path, param):
         print(path)
-        origin_path = ''
+        # if it is url format
+        http_url_pattern = re.compile(r'^(/[^/]*)*(\?.*)?$')
+        if not bool(re.match(http_url_pattern, path)):
+            self.handle_error(400, 'Bad Request')
+            return
         # modify to actual path
         if path == '/':
             origin_path = 'data'
@@ -122,21 +143,49 @@ class HttpServer:
 
         # if the path is a directory, return the index.html file
         if os.path.isdir(origin_path):
-            response_body = list_directory_html(origin_path, path)
-            response = f'HTTP/1.1 200 OK\r\n\r\n{response_body}'
-            self.client_socket.sendall(response.encode("utf-8"))
+            if param == '':
+                self.handle_dir(origin_path, path, True)
+            else:
+                self.handle_error(405, 'Method Not Allowed')
         # if the path is a file, return the file
-        elif os.path.isfile( origin_path):
+        elif os.path.isfile(origin_path):
             self.handle_file(path, is_head=True)
         else:
             self.handle_error(404, 'Not Found')
 
+    def handle_dir(self, origin_path, web_path, is_html, is_head=False):
+        response_body = []
+        response_header = ''
+        dir_time = datetime.datetime.fromtimestamp(os.path.getmtime(origin_path)).strftime('%a, %d %b %Y %H:%M:%S GMT')
+        if is_html:
+            response_body = list_directory_html(origin_path, web_path)
+            dir_size = len(response_body)
+            dir_type = 'text/html'
+            response_header = '{} 200 OK\r\nContent-Length: {}\r\nContent-Type: {}\r\nLast-Modified: {}\r\n\r\n'.format(
+                'HTTP/1.1', dir_size, dir_type, dir_time)
+        else:
+            with os.scandir(origin_path) as entries:
+                for entry in entries:
+                    if entry.is_dir():
+                        response_body.append(f'{entry.name}/')
+                    else:
+                        response_body.append(f'{entry.name}')
+            response_body = str(response_body)
+            dir_size = len(response_body)
+            dir_type = 'text/plain'
+            response_header = '{} 200 OK\r\nContent-Length: {}\r\nContent-Type: {}\r\nLast-Modified: {}\r\n\r\n'.format(
+                'HTTP/1.1', dir_size, dir_type, dir_time)
+        self.client_socket.sendall(response_header.encode("utf-8"))
+        if not is_head:
+            self.client_socket.sendall(response_body.encode('utf-8'))
+
     def handle_file(self, path, is_head=False):
-        extension = pathlib.Path(path).suffix # 拓展名
+        extension = pathlib.Path(path).suffix  # 拓展名
         file_size = os.path.getsize(path)
         file_type = mimetypes.types_map[extension]
         file_time = datetime.datetime.fromtimestamp(os.path.getmtime(path)).strftime('%a, %d %b %Y %H:%M:%S GMT')
-        response_header = '{} 200 OK\r\nContent-Length: {}\r\nContent-Type: {}\r\nLast-Modified: {}\r\n\r\n'.format('HTTP/1.1', file_size, file_type, file_time)
+        response_header = '{} 200 OK\r\nContent-Length: {}\r\nContent-Type: {}\r\nLast-Modified: {}\r\n\r\n'.format(
+            'HTTP/1.1', file_size, file_type, file_time)
         self.client_socket.sendall(response_header.encode('utf-8'))
 
         # send the file
@@ -167,42 +216,72 @@ class HttpServer:
         self.client_socket.sendall(response_header.encode('utf-8'))
         self.client_socket.sendall(error_message.encode('utf-8'))
 
+
 def list_directory_html(origin_path, web_path):
+    html_content = f"""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+    <html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Directory listing for ./{origin_path}</title>
+    </head>
+    <body>
+    <h1>Directory listing for ./{origin_path}</h1>
+    <hr>
+    <ul>
+    """
     try:
-        with os.scandir(origin_path) as entries:
-            files_and_dirs = [entry.name for entry in entries]
         links = []
         parent_path = os.path.dirname(web_path)
         print(origin_path)
         print(parent_path)
-        if origin_path != 'data':
-            links.extend([f'<a href="/">/</a>', f'<a href="{parent_path}">../</a>'])
-            links.extend([f'<a href="{web_path[1:]}/{entry}">{entry}</a>' for entry in files_and_dirs])
-        else:
-            links.extend([f'<a href="{web_path[1:]}{entry}">{entry}</a>' for entry in files_and_dirs])
+        with os.scandir(origin_path) as entries:
+            if origin_path != 'data':
+                links.extend([f'<a href="/">/</a>', f'<a href="{parent_path}">../</a>'])
+                for entry in entries:
+                    if entry.is_dir():
+                        links.append(f'<li><a href="{web_path[1:]}/{entry.name}">{entry.name}/</a></li>')
+                    else:
+                        links.append(f'<li><a href="{web_path[1:]}/{entry.name}">{entry.name}</a></li>')
+            else:
+                for entry in entries:
+                    if entry.is_dir():
+                        links.append(f'<li><a href="{web_path[1:]}{entry.name}">{entry.name}/</a></li>')
+                    else:
+                        links.append(f'<li><a href="{web_path[1:]}{entry.name}">{entry.name}</a></li>')
         print(links)
+        html_content += '\r\n'.join(links)
         # 合并所有链接
-        return "<br>".join(links)
+        html_content1 = """
+        </ul>
+        <hr>
+        </body>
+        </html>
+        """
+        html_content += html_content1
+        return html_content
     except FileNotFoundError:
         return "Directory not found"
     except Exception as e:
         return f"Error: {str(e)}"
 
+
 terminate_flag = False
+
 
 def signal_handler(sig, frame):
     global terminate_flag
     print('Ctrl+C pressed. Exiting gracefully...')
     terminate_flag = True
 
+
 signal.signal(signal.SIGINT, signal_handler)
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 8080)) # bind the socket to host and port
+server_socket.bind(('localhost', 8080))  # bind the socket to host and port
 
 while not terminate_flag:
-    server_socket.listen(5) # start listening for incoming connections, 5 is the max number of queued connections
-    client_socket, addr = server_socket.accept() # accept the connection, addr is the address bound to the socket on the other end of the connection, including the port number and IP address
+    server_socket.listen(5)  # start listening for incoming connections, 5 is the max number of queued connections
+    client_socket, addr = server_socket.accept()  # accept the connection, addr is the address bound to the socket on the other end of the connection, including the port number and IP address
     print('Got a connection from {}'.format(addr))
 
     # create a new thread to handle the request
